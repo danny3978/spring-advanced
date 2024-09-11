62d0ac3b92cb89539140e2a9588e9bf9187b0614
dam <ekadl025@gmail.com>
2024-09-11 11:38:58 +0900

    필수 기능: level.2 - 5 (완) 수정

--- src/test/java/org/example/expert/UserAdminControllerTest.java
+++ src/test/java/org/example/expert/UserAdminControllerTest.java
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.expert.aop.AspectAdmin;
 import org.example.expert.config.FilterConfig;
 import org.example.expert.config.JwtUtil;
-import org.example.expert.domain.user.controller.UserAdminController;
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.expert.domain.user.service.UserAdminService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -19,7 +17,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,7 +26,7 @@ public class UserAdminControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private UserAdminService userAdminService;
 
     @Autowired
@@ -43,7 +40,7 @@ public class UserAdminControllerTest {
         // given
         Long userId = 1L;
         UserRoleChangeRequest request = new UserRoleChangeRequest();
-        request.setNewRole("ROLE_ADMIN"); // 예시 데이터
+        request.setNewRole("ROLE_ADMIN");
 
         String jsonRequest = objectMapper.writeValueAsString(request);
 
--- src/test/java/org/example/expert/domain/CommentAdminControllerTest.java
+++ src/test/java/org/example/expert/domain/CommentAdminControllerTest.java
@@ -3,21 +3,15 @@ package org.example.expert.domain;
 import org.example.expert.aop.AspectAdmin;
 import org.example.expert.config.FilterConfig;
 import org.example.expert.config.JwtUtil;
-import org.example.expert.domain.comment.controller.CommentAdminController;
 import org.example.expert.domain.comment.service.CommentAdminService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
